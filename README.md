a56d9dfb89d3f5e52e6865d85c8ea75350afce95
[Ivo Pletikosic] <[ifpleti@gmail.com]>
2021-02-06 03:08:24 -0300

    Repo's birth

---
+++ README.md
@@ -0,0 +1,6 @@
+pre-req:
+- git
+- zsh
+- https://ohmyz.sh/
+- https://github.com/zsh-users/zsh-autosuggestions
+
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+# variables
+ZSH_DIR=${ZSH:-~/.dots}
+REPO=${REPO:-ifpleti/dots}
+REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+BRANCH=${BRANCH:-master}
+DOTFILES_DIR=~/.dots/dotfiles
+
+# exception
+fmt_error() {
+    echo "Error!!!: $@" >&2
+}
+
+# clone repo
+git clone -c core.eol=lf -c core.autocrlf=false \
+-c fsck.zeroPaddedFilemode=ignore \
+-c fetch.fsck.zeroPaddedFilemode=ignore \
+-c receive.fsck.zeroPaddedFilemode=ignore \
+--depth=1 --branch "$BRANCH" "$REMOTE" "$ZSH_DIR" || {
+    fmt_error "git clone of dots repo failed"
+    exit 1
+}
+echo
+
+# delete existing dotfiles
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.oh-my-zsh/themes/shades-of-purple.zsh-theme > /dev/null 2>&1
+
+# create soft links
+ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
+ln -sf $DOTFILES_DIR/.oh-my-zsh/themes/shades-of-purple.zsh-theme ~/.oh-my-zsh/themes/shades-of-purple.zsh-theme
