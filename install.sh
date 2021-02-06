2031f563f715cadb07fa10baebd1f103fde3e20a
ifpleti <ifpleti@gmail.com>
2021-02-06 19:54:02 -0300

    intall.sh update

--- install.sh
+++ install.sh
@@ -35,6 +35,6 @@ ln -sf $DOTFILES_DIR/init.vim ~/.config/nvim/init.vim
 
 # install vim pluggins
 echo "Installing Vim pluggins..."
-nvim -Es -u "$HOME/.config/nvim/init.vim" +PlugClean +PlugInstall > /dev/null 2>&1
+nvim -Es -u "$HOME/.config/nvim/init.vim" +PlugClean +PlugInstall > /dev/null 2>&1 || true
 
 echo "Done."
